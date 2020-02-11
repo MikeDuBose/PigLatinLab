aac6368255a650c3a837fb546cbe7798c8c01844
Michael DuBose <michaeldubose@quickenloans.com>
2020-02-11 06:45:53 -0500

    Pig latin lab

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PigLatinLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, and welcome to pig latin!  Enter a word: ");
+            string input = Console.ReadLine().ToLower();
+            string vowel = "aeiou";
+            string[] inputArray = input.Split(" ");
+
+            foreach (string w in inputArray)
+            {
+                if (vowel.Contains(w[0]))
+                {
+                    Console.Write(Vowel(w) + " ");
+                }
+                else
+                {
+                    Console.Write(Consonant(w) + " ");
+                }
+            }
+        }
+        private static string Consonant(string input)
+        {
+            string consonant = "bcdfghjklmnpqrstvwxyz";
+            Stack<char> cStack = new Stack<char>();
+            //Must initialize the output, or else you can't "+=" to it
+            string output = "";
+            int i = 0;
+            while (consonant.Contains(input[i]))
+            {
+                cStack.Push(input[i]);
+                i++;
+            }
+            for (int j = 0; j < (input.Length - j); j++)
+            {
+                output += input[j + j];
+            }
+            output += cStack.Pop();
+
+            return (output + "ay");
+        }
+
+        private static string Vowel(string input)
+        {
+            return (input + "way");
+        }
+    }
+}
